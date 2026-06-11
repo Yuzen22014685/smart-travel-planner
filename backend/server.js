ffc0d2574ada00040abedfdb9cb28cb315df54b8
xuan-wei9900 <lowxuanwei29@gmail.com>
2026-06-11 15:31:05 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
+const mongoose = require("mongoose");
 const express = require('express');
+
 const app = express();
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const weatherRoutes = require('./routes/weatherRoutes');
 
 app.use('/api/weather', weatherRoutes);
 
-app.use(express.json());
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
